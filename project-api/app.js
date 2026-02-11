295cad57502c947613e874233b4d9df0fecce09f
Jose Salgado <joseignaciosalgado@MacBook-Pro-de-Jose.local>
2026-02-11 16:02:54 -0300

    Increase express body limit to 10mb

--- project-api/app.js
+++ project-api/app.js
@@ -42,7 +42,8 @@ app.use(
 
 app.options('*', cors());
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 
 if (process.env.DISABLE_CSRF !== 'true') {
